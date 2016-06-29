404bc1c0475f0368d2126b205478f28a11a6d2d1
dapias <dandroveros@gmail.com>
2016-06-29 14:02:36 -0500

    funciones del producto punto

---
+++ ProductoPunto.jl
@@ -0,0 +1,31 @@
+module ProductoPunto
+
+export miproducto
+
+function miproducto(a::Vector{Float64},b::Vector{Float64})
+    if length(a) == length(b)
+        suma = 0.0
+        for i in 1:length(b)
+        suma += a[i]*b[i]
+        end
+     return suma
+
+    else
+        #println("Las dimensiones no son buenas")
+    #    error("Las dimensiones no son buenas")
+        throw(DimensionMismatch("Las dimensiones no son buenas"))
+    end
+end
+
+function miproducto2(a::Vector{Float64},b::Vector{Float64})
+  if length(a) == length(b)
+    suma = sum(a.*b)
+    return suma
+  else
+      #println("Las dimensiones no son buenas")
+      #    error("Las dimensiones no son buenas")
+      throw(DimensionMismatch("Las dimensiones no son buenas"))
+    end
+end
+
+end
